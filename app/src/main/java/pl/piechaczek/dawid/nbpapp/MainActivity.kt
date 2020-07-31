02e6bf6012245f98cbd2f4348ea810e81ab2ed7f
Dawid Piechaczek <d.piechaczek@bsgroup.eu>
2020-07-31 14:28:42 +0200

    NBP-2: Core module added.

--- app/src/main/java/pl/piechaczek/dawid/nbpapp/MainActivity.kt
+++ app/src/main/java/pl/piechaczek/dawid/nbpapp/MainActivity.kt
@@ -1,2 +1,13 @@
 package pl.piechaczek.dawid.nbpapp
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+}
